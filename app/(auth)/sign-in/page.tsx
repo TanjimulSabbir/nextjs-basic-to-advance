a3001a0631913c2f82b1116611247d82dcb30aa2
Tanjimul Islam Sabbir <86679380+TanjimulSabbir@users.noreply.github.com>
2025-09-18 01:04:06 +0600

    home page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-
 import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/Validations";
 import { signInWithCredentials } from "@/lib/actions/auth.action";
-import { SignInSchema } from "@/lib/validations";
+
 
 const SignIn = () => {
   return (
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,49 @@
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Dev Overflow | Home",
+  description:
+    "Discover different programming questions and answers with recommendations from the community.",
+};
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-      <Button
-        className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
-        asChild
-      >
-        <Link href={ROUTES.ASK_QUESTION} className="max-sm:w-full">
-          Ask a Question
-        </Link>
-      </Button>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION} className="max-sm:w-full">
+            Ask a Question
+          </Link>
+        </Button>
+      </section>
+
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.HOME}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          iconPosition="left"
+          otherClasses="flex-1"
+        />
+
+        {/* <CommonFilter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        /> */}
+      </section>
+
+      
+
+    
+    </>
   );
 }
