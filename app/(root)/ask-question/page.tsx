4cac57280991a6eee4a439ba65c2dec81ea63552
Tanjimul Islam Sabbir <86679380+TanjimulSabbir@users.noreply.github.com>
2025-09-19 23:19:32 +0600

    mongoose setup

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,17 @@
-import QuestionForm from '@/components/forms/QuestionForm'
-import React from 'react'
+import React from "react";
 
-export default function AskQuestion() {
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const AskAQuestion = () => {
   return (
-      <div>
-          <h1>Ask Question</h1>
-          <QuestionForm/>
-    </div>
-  )
-}
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
+};
+
+export default AskAQuestion;
